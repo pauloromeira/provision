00e07ec2590b0e57a5ff9fc518c3a0bc9adae9a0
Paulo Romeira <paulo@pauloromeira.com>
2022-05-26 13:59:47 -0300

    provision: Tweak

--- provision.sh
+++ provision.sh
@@ -6,6 +6,7 @@ CMND="${1}"
 ARGS="${@:2}"
 SUDO=""
 
+[ "${EUID}" -ne 0 ] && SUDO="sudo "
 [[ "$#" -eq 0 || "${CMND}" = "bootstrap" ]] && CMND="pull"
 [ "${CMND}" = "pull" ] && CMND="ansible-pull -U ${REPO}"
 [ "${CMND}" = "sync" ] && CMND="ansible-pull -o -U ${REPO}"
@@ -13,18 +14,14 @@ SUDO=""
 [ "${CMND}" = "local" ] && CMND="ansible-playbook local.yml"
 [ "${*}" = "setup" ] && CMND="ansible localhost -m setup"
 
-if [ "${EUID}" -ne 0 ]; then
-  SUDO="sudo "
-  export ANSIBLE_BECOME_ASK_PASS="True"
-fi
-
 # Dependencies: debian
 (command -v dpkg && dpkg -s ${DEPS[@]} || ! command -v apt-get) &> /dev/null \
   || (${SUDO}apt-get update && ${SUDO}apt-get install -y ${DEPS[@]})
 
 PYTHON="$(command -v python3 || command -v python)"
-export ANSIBLE_PYTHON_INTERPRETER="${PYTHON}"
 export PATH="${HOME}/.local/bin:${PATH}"
+export ANSIBLE_PYTHON_INTERPRETER="${PYTHON}"
+[ -n "${SUDO}" ] && export ANSIBLE_BECOME_ASK_PASS="True"
 
 "${PYTHON}" -m pip install --user --disable-pip-version-check pipx > /dev/null \
   && "${PYTHON}" -m pipx install ansible --include-deps > /dev/null \
