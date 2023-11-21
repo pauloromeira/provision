2df299361ddc74fb5dd72367d1ba25e146dbf2a6
Paulo Romeira <paulo@pauloromeira.com>
2023-11-21 13:29:24 -0300

    provision: Add fedora deps

--- provision.sh
+++ provision.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 REPO="https://github.com/pauloromeira/provision.git"
-DEPS=(git python3 python3-pip python3-venv)
 VENV="${HOME}/.local/pipx/venvs/ansible"
 CMND="${1}"
 ARGS="${@:2}"
 SUDO=""
+DEBIAN_DEPS=(git python3 python3-pip python3-venv)
+FEDORA_DEPS=(git python3 python3-pip)
 
 [ "${EUID}" -ne 0 ] && SUDO="sudo "
 [[ "$#" -eq 0 || "${CMND}" = "bootstrap" ]] && CMND="pull"
@@ -15,8 +16,12 @@ SUDO=""
 [ "${*}" = "setup" ] && CMND="ansible localhost -m setup"
 
 # Dependencies: debian
-(command -v dpkg && dpkg -s ${DEPS[@]} || ! command -v apt-get) &> /dev/null \
-  || (${SUDO}apt-get update && ${SUDO}apt-get install -y ${DEPS[@]})
+(command -v dpkg && dpkg -s ${DEBIAN_DEPS[@]} || ! command -v apt-get) &> /dev/null \
+  || (${SUDO}apt-get update && ${SUDO}apt-get install -y ${DEBIAN_DEPS[@]})
+
+# Dependencies: fedora
+(command -v rpm && rpm -q ${FEDORA_DEPS[@]} || ! command -v dnf) &> /dev/null \
+  || ${SUDO}dnf install -y ${FEDORA_DEPS[@]}
 
 PYTHON="$(command -v python3 || command -v python)"
 export PATH="${HOME}/.local/bin:${PATH}"
