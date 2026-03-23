dd6e1c5cf90f7fce3b063ec41968c810b07f4ee3
Paulo Romeira <paulo@pauloromeira.com>
2026-03-23 19:04:39 -0300

    provision: Install pipx from distro package manager and other tweaks

--- provision.sh
+++ provision.sh
@@ -3,8 +3,8 @@ REPO="https://github.com/pauloromeira/provision.git"
 CMND="${1}"
 ARGS="${@:2}"
 SUDO=""
-DEBIAN_DEPS=(git python3 python3-pip python3-venv)
-FEDORA_DEPS=(git python3 python3-pip)
+DEBIAN_DEPS=(git python3 python3-venv pipx)
+FEDORA_DEPS=(git python3 pipx)
 
 [ "${EUID}" -ne 0 ] && SUDO="sudo "
 [[ "$#" -eq 0 || "${CMND}" = "bootstrap" ]] && CMND="pull"
@@ -23,11 +23,9 @@ FEDORA_DEPS=(git python3 python3-pip)
   || ${SUDO}dnf install -y ${FEDORA_DEPS[@]}
 
 PYTHON="$(command -v python3 || command -v python)"
-export PATH="${HOME}/.local/bin:${PATH}"
 export ANSIBLE_PYTHON_INTERPRETER="${PYTHON}"
 [ -n "${SUDO}" ] && export ANSIBLE_BECOME_ASK_PASS="True"
 
-"${PYTHON}" -m pip install --user --disable-pip-version-check pipx > /dev/null \
-  && "${PYTHON}" -m pipx install ansible --include-deps > /dev/null \
-  && export $("${PYTHON}" -m pipx environment | grep "^PIPX_LOCAL_VENVS=") \
-  && "${PIPX_LOCAL_VENVS}/ansible/bin/"${CMND} ${ARGS[@]}
+pipx ensurepath > /dev/null \
+  && pipx install --python "${PYTHON}" ansible --include-deps > /dev/null \
+  && pipx run --spec ansible ${CMND} ${ARGS[@]}
